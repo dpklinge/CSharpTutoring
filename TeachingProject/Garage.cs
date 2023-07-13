0d675c44a5f95879cff7dd6ea4044e3f05dd8da6
Daniel Klingensmith <danielpklingensmith@gmail.com>
2023-07-13 15:20:35 -0400

    Initial lesson

---
+++ TeachingProject/Garage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachingProject
+{
+    public class Garage
+    {
+        public List<Vehicle> vehicles = new List<Vehicle>();
+
+        public void StoreVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+    }
+}
---
+++ TeachingProject/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TeachingProject;
+
+Console.WriteLine("Starting the program");
+Vehicle myVehicle = new Vehicle(1,2,3);
+Vehicle myVehicle2 = new Vehicle();
+var amountToMove = 5;
+myVehicle.GoForwards(amountToMove);
+myVehicle.GoForwards(.1f);
+
+Garage myGarage = new Garage();
+myGarage.StoreVehicle(myVehicle);
+
+Console.WriteLine("Hello, World! My sum is: " );
+
+public class Vehicle
+{
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+    public Vehicle(int xProvided, int yProvided, int zProvided) {
+        x = xProvided;
+        y = yProvided;
+        z = zProvided;
+    }
+    public Vehicle()
+    {
+    }
+
+    public void GoForwards(int amount)
+    {
+        z += amount;
+    }
+
+    public void GoForwards(float amount)
+    {
+        z = (int)(z + amount);
+    }
+}
+
