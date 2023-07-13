9758b748fd9990a1fb0b7164b0f9d9289a71ae2e
Daniel Klingensmith <danielpklingensmith@gmail.com>
2023-07-13 15:28:21 -0400

    Added comments

--- TeachingProject/Program.cs
+++ TeachingProject/Program.cs
@@ -2,22 +2,27 @@
 using TeachingProject;
 
 Console.WriteLine("Starting the program");
+// 'new Vehicle()' calls the constructor in the Vehicle class to create a new Vehicle object.
+// You won't have a vehicle at all until new Vehicle is called at least once!
 Vehicle myVehicle = new Vehicle(1,2,3);
 Vehicle myVehicle2 = new Vehicle();
 var amountToMove = 5;
+// Tells the first vehicle we made to go forwards 5. Note that myVehicle2 will remain unaffected -
+// each separate new Vehicle will maintain its own independent position
 myVehicle.GoForwards(amountToMove);
 myVehicle.GoForwards(.1f);
 
 Garage myGarage = new Garage();
 myGarage.StoreVehicle(myVehicle);
 
-Console.WriteLine("Hello, World! My sum is: " );
 
 public class Vehicle
 {
     public int x = 0;
     public int y = 0;
     public int z = 0;
+
+    //A constructor - Constructors will have the same name as the class, and no return type
     public Vehicle(int xProvided, int yProvided, int zProvided) {
         x = xProvided;
         y = yProvided;
@@ -29,12 +34,12 @@ public class Vehicle
 
     public void GoForwards(int amount)
     {
-        z += amount;
+        z += amount; // Equivalent to z = z+amount;
     }
 
     public void GoForwards(float amount)
     {
-        z = (int)(z + amount);
+        z = (int)(z + amount); // (int) is a 'cast' - it forces (z+amount) to be converted from float to int
     }
 }
 
